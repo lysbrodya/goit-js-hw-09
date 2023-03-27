04add8ea1a465bd0d296cad2467878bee393b1ef
lysbrodya <110303676+lysbrodya@users.noreply.github.com>
2023-03-27 17:27:27 +0000

    Deploying to gh-pages from @ lysbrodya/goit-js-hw-09@a04721e227e1b45ca3b985d772491d79fa045a7c 🚀

---
+++ 01-color-switcher.9a9c9a7f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),a=document.querySelector("body");e.addEventListener("click",(()=>{changeColor=setInterval((()=>a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(()=>{clearInterval(changeColor),t.disabled=!0,e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.9a9c9a7f.js.map
